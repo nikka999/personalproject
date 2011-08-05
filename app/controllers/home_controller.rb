c7d0a104df224293749bc63abfba25a5152a093e
Nikka Ghalili <nikka999@gmail.com>
2011-08-05 00:50:32 -0400

    log in for video

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  helper_method :current_user
+  
   def index
   end
+    
+  private
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
